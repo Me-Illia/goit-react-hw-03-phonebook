3669f15d5a4d87add7a3d94386807352f561049a
Me-Illia <sq999@ukr.net>
2023-06-13 22:47:40 +0300

    -

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -51,20 +51,6 @@ class App extends Component {
     }));
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
   render() {
     return (
       <div
